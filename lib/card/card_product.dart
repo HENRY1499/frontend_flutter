e25f8b04bd6a90873777e5aed9b09d918a90f4f0
Henry Castillo <hdaniel031499@gmail.com>
2025-06-18 16:10:24 -0500

    cardCustomer

--- lib/card/card_product.dart
+++ lib/card/card_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_tecnica/card/customCard.dart';
 
 class CardProduct extends StatefulWidget {
   const CardProduct({super.key});
@@ -11,75 +12,38 @@ class _CardProduct extends State<CardProduct> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Producto"),
-        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(2),
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Container(
-                height: 200,
-                padding: EdgeInsets.all(8),
-                margin: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.blueAccent,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(children: [Text("Producto 1")]),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomCard(
+                name: "Henry",
+                description: "Lorem Ipsum",
+                number: 26,
+                color: Colors.red,
+                icon: Icons.star,
+                image: "https://picsum.photos/200/300?random=1",
               ),
-            ),
-            Expanded(
-              child: Container(
-                height: 200,
-                padding: EdgeInsets.all(8),
-                margin: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.blueAccent,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(children: [Text("Producto 1")]),
+              CustomCard(
+                name: "Henry",
+                description: "Lorem Ipsum",
+                number: 26,
+                color: Colors.blue,
+                icon: Icons.star,
+                image: "https://picsum.photos/200/300?random=2",
               ),
-            ),
-            Expanded(
-              child: Container(
-                height: 200,
-                padding: EdgeInsets.all(8),
-                margin: EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.blueAccent,
-                      blurRadius: 4,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: Column(children: [Text("Producto 1")]),
+              CustomCard(
+                name: "Henry",
+                description: "Lorem Ipsum",
+                number: 26,
+                color: Colors.green,
+                icon: Icons.star,
+                image: "https://picsum.photos/200/300?random=3",
               ),
-            ),
-            // Expanded(child: Column(children: [Text("Producto 1")])),
-            // Expanded(child: Column(children: [Text("Producto 1")])),
-          ],
+            ],
+          ),
         ),
       ),
     );
