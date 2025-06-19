d00c7e370af674e2c8d4756c8e7b8b6c21b3920b
HENRY1499 <hdaniel031499@gmail.com>
2025-06-18 23:00:57 -0500

    .

--- lib/presentation/screens/Product.dart
+++ lib/presentation/screens/Product.dart
@@ -16,42 +16,64 @@ class _ProductState extends State<Product> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(60.0),
         child: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black45),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
           backgroundColor: const Color(0xff5A3D29),
-          title: AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: isExpanded ? screenWidth * 0.8 : screenWidth * 0.1,
-            height: 40.0,
-            child: SearchBar(
-              hintText: 'Buscar Producto ',
-              hintStyle: WidgetStateProperty.all(
-                TextStyle(
-                  color: Colors.grey,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 16,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text(
+                    "Hola",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: isExpanded ? screenWidth * 0.5 : screenWidth * 0.1,
+                height: 40.0,
+                child: SearchBar(
+                  hintText: 'Buscar Producto ',
+                  hintStyle: WidgetStateProperty.all(
+                    TextStyle(
+                      color: Colors.grey,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  shadowColor: WidgetStateProperty.all(Colors.grey[800]),
+                  backgroundColor:
+                      isExpanded
+                          ? WidgetStateProperty.all(Color(0xffE9D7C0))
+                          : WidgetStateProperty.all(
+                            Color.fromARGB(162, 233, 215, 192),
+                          ),
+                  elevation: WidgetStateProperty.all(0.7),
+                  leading: Icon(Icons.search, color: Colors.white),
+                  onTap: () {
+                    setState(() {
+                      isExpanded = true;
+                    });
+                  },
+                  onTapOutside: (PointerDownEvent event) {
+                    FocusScope.of(context).unfocus();
+                    setState(() {
+                      isExpanded = false;
+                    });
+                  },
                 ),
               ),
-              shadowColor: WidgetStateProperty.all(Colors.grey[800]),
-              backgroundColor: WidgetStateProperty.all(Color(0xffE9D7C0)),
-              elevation: WidgetStateProperty.all(0.7),
-              leading: Icon(Icons.search, color: Colors.white),
-              onTap: () {
-                setState(() {
-                  isExpanded = true;
-                });
-              },
-              onTapOutside: (PointerDownEvent event) {
-                FocusScope.of(context).unfocus();
-                setState(() {
-                  isExpanded = false;
-                });
-              },
-            ),
+            ],
           ),
         ),
       ),
